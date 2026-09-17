a9f61b52b4926187e6fbd67f32f7905c64bafc1f
Joseph Akitoye <josepha@molaprise.com>
2026-09-17 16:22:09 -0400

    docs(etilize): add documentation to etilize service provider
    Signed-off-by: Joseph Akitoye <josepha@molaprise.com>

--- src/Contracts/EtilizeCatalogServiceInterface.php
+++ src/Contracts/EtilizeCatalogServiceInterface.php
@@ -4,15 +4,40 @@ namespace Molaprise\Molasync\Contracts;
 
 interface EtilizeCatalogServiceInterface
 {
+    /**
+     * @param array $identifiers
+     * @return int|null
+     */
     public function resolveProductId(array $identifiers): ?int;
 
+    /**
+     * @param array $identifiers
+     * @return array
+     */
     public function resolveProductIdWithDiagnostics(array $identifiers): array;
 
+    /**
+     * @param string|null $manufacturerPartNumber
+     * @return int|null
+     */
     public function resolveProductIdByManufacturerPartNumber(?string $manufacturerPartNumber): ?int;
 
+    /**
+     * @param string|null $upc
+     * @return int|null
+     */
     public function resolveProductIdByUpc(?string $upc): ?int;
 
+    /**
+     * @param int $productId
+     * @return array
+     */
     public function similarProductIds(int $productId): array;
 
+    /**
+     * @param string $query
+     * @param int $limit
+     * @return array
+     */
     public function searchProductIds(string $query, int $limit = 500): array;
 }
